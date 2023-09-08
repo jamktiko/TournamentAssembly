2435685c6e4730732e02ab40bc70fc5ec9299f79
EemiSalonen <eemi.salonen@gmail.com>
2023-09-08 23:30:17 +0300

    created tourview-component

---
+++ src/app/tourselector/tourselector.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tourselector',
+  templateUrl: './tourselector.component.html',
+  styleUrls: ['./tourselector.component.css']
+})
+export class TourselectorComponent {
+
+}
