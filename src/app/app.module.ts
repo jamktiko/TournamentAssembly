7833a45248cae4c031ea4933b2ec14a2297d8a65
Eemi Salonen <ab6905@student.jamk.fi>
2023-09-13 12:48:32 +0300

    started work on profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TourviewComponent } from './tourview/tourview.component';
 import { FormsModule } from '@angular/forms';
-import { TourselectorComponent } from './tourselector/tourselector.component';
 import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
-  declarations: [AppComponent, TourviewComponent, TourselectorComponent, ProfileComponent],
+  declarations: [AppComponent, ProfileComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
