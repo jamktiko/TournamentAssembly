ddefe51dad9d123ea465187d462a2eaa335308f7
Eemi Salonen <ab6905@student.jamk.fi>
2023-09-13 12:20:38 +0300

    deleted testing folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TourviewComponent } from './tourview/tourview.component';
 import { FormsModule } from '@angular/forms';
-import { TourselectorComponent } from './tourselector/tourselector.component';
 
 @NgModule({
-  declarations: [AppComponent, TourviewComponent, TourselectorComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
