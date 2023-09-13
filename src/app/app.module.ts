fddce7e7ca0303b9878ef70410b291b91ddbccaf
mikkohaemaelaeinen <ab7778@student.jamk.fi>
2023-09-13 12:44:27 +0300

    button component folder added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ButtonComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
